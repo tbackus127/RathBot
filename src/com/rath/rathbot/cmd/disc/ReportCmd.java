1ffaa78c6163e7023321d6bc5b6c6ec9f5736f8b
lehenbnw <lehenbnw@gmail.com>
2018-04-30 19:01:50 -0500

    Started work on ReportCmd TODOs, work incomplete.

--- src/com/rath/rathbot/cmd/disc/ReportCmd.java
+++ src/com/rath/rathbot/cmd/disc/ReportCmd.java
@@ -1,12 +1,17 @@
 
 package com.rath.rathbot.cmd.disc;
 
+import com.rath.rathbot.RathBot;
 import com.rath.rathbot.cmd.RBCommand;
 
+import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IUser;
 
 public class ReportCmd extends RBCommand {
+  
+  private static final long REPORT_CHANNEL_ID = 291441076257161217L;
+  private static IDiscordClient client = RathBot.getClient();
   
   @Override
   public String getCommandName() {
@@ -38,9 +43,13 @@ public class ReportCmd extends RBCommand {
     
     System.out.println("Executing report");
     
-    // TODO: Reply that the report has been received
+    RathBot.sendDirectMessage(author, "Thank you for your report! We will look into this and take action accordingly.");
+    
     // TODO: Log report
+    
     // TODO: Post to #report
+    IChannel report = ReportCmd.client.getChannelByID(ReportCmd.REPORT_CHANNEL_ID);
+    RathBot.sendMessage(report, "User " + author.getName());
     
     return RBCommand.STOP_CMD_SEARCH;
   }
